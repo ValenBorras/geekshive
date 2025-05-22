a70862eb7804a69086e5fb47a5ed27921f899e7d
Valentina <mvalentina.borras@gmail.com>
2025-05-22 12:50:11 -0300

    redirected old urls to /

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/es/historia.html',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/es/argentina/arg_buenosaires.html',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/es/LaComunidad.html',
+        destination: '/',
+        permanent: true,
+      }
+    ];
+  },
+};
 
 export default nextConfig;
